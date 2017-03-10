048cab86de02615254c2003f92bcc143a65d49b4
iampmat <patrickmatherly1994@gmail.com>
2017-03-09 19:40:05 -0500

    Add sound mp3 and logic involving Timer class

--- BedButtonAlarm/ViewController.swift
+++ BedButtonAlarm/ViewController.swift
@@ -7,10 +7,18 @@
 //
 
 import UIKit
+import AVFoundation
+import CoreMotion
 
 class ViewController: UIViewController {
 
     @IBOutlet var time: UIDatePicker!
+    
+    var timer = Timer()
+    
+    var player: AVAudioPlayer?
+    
+    var motionManager: CMMotionManager!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,11 +30,47 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
-    @IBAction func setTimer(_ sender: Any) {
+    @IBAction func startTimer(_ sender: Any) {
+        let timeInterval = time.countDownDuration
+        self.timer = Timer.scheduledTimer(timeInterval: timeInterval,
+                                          target: self,
+                                          selector: #selector(timerDidEnd(timer:)),
+                                          userInfo: "Wake up fool",
+                                          repeats: false)
     }
 
     @IBAction func stopTimer(_ sender: Any) {
+        self.timer.invalidate()
+    }
+    
+    func timerDidEnd(timer:Timer) {
+        self.playSound()
+        self.motionManager = CMMotionManager()
+        
+        if self.motionManager.isAccelerometerAvailable {
+            self.motionManager.accelerometerUpdateInterval = 0.01
+            self.motionManager.startAccelerometerUpdates(to: OperationQueue.main, withHandler: {(accelData: CMAccelerometerData?, errorOC: Error?) in
+                print("\(accelData?.acceleration.x) \(accelData?.acceleration.y) \(accelData?.acceleration.z)")
+            })
+        }
+    }
+    
+    func playSound() {
+        let url = Bundle.main.url(forResource: "alarm", withExtension: "mp3")!
+        
+        do {
+            self.player = try AVAudioPlayer(contentsOf: url)
+            guard let player = player else { return }
+            
+            player.prepareToPlay()
+            player.play()
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func stopSound() {
+        self.player?.pause()
     }
 }
 
